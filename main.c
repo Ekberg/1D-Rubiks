bd6c6d3ea48d19e8adc403aab716a909eb54a114
Anton Ekberg <anton.ekberg@gmail.com>
2012-11-12 16:34:48 +0100

    Added some error handling and allowing the initial cube to be changed by argv

--- main.c
+++ main.c
@@ -2,10 +2,16 @@
 #include <string.h>
 #include "cube.h"
 
+#define DEFAULT_CUBE "12345678"
+
 const char* PROGRAM_NAME;
 
 int main(int argc, char **argv) {
+	char* initial_cube;
 	char* sequence;
+	
+	
+	
 
 	if (argc) {
 		if (argv[0][0] == '.' && argv[0][1] == '/') {
@@ -19,14 +25,36 @@ int main(int argc, char **argv) {
 		argv++;
 		
 		if (is_valid_sequence(*argv)) {
+			if (sequence) {
+				printf("Invalid argument, already got a sequence.\n");
+				return -1;
+			}
 			sequence = *argv;
+		} else if (is_valid_cube_data(*argv)) {
+			if (initial_cube) {
+				printf("Invalid argument, already got a initial cube.\n");
+				return -1;
+			}
+			initial_cube = *argv;
+			
 		} else {
 			printf("Invalid argument.\n");
 			return -1;
 		}
 	}
+	
+	if (!sequence) {
+		printf("Not enough arguments.\n");
+		return -1;
+	}
+	
+	if (!initial_cube) {
+		initial_cube = DEFAULT_CUBE;
+	}
+	
+	
 
-	cube_t* cube = cube_create("12345678");
+	cube_t* cube = cube_create(initial_cube);
 	
 	cube_transform(cube, sequence);
 	
