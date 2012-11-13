3a5074d66d5bfa7644103aaf72df33f2fc4c6800
Anton Ekberg <anton.ekberg@gmail.com>
2012-11-13 09:41:05 +0100

    Added command-line support for brute-force

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC = clang
-FILES = cube.c main.c
+FILES = cube.c brute.c main.c
 OUTPUT = rubiks1d
 
 build: $(FILES)
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "cube.h"
+#include "brute.h"
 
 #define DEFAULT_CUBE "12345678"
 
@@ -8,11 +9,9 @@ const char* PROGRAM_NAME;
 
 int main(int argc, char **argv) {
 	char* initial_cube;
+	char* target_cube;
 	char* sequence;
 	
-	
-	
-
 	if (argc) {
 		if (argv[0][0] == '.' && argv[0][1] == '/') {
 			PROGRAM_NAME = &argv[0][2];
@@ -31,35 +30,42 @@ int main(int argc, char **argv) {
 			}
 			sequence = *argv;
 		} else if (is_valid_cube_data(*argv)) {
-			if (initial_cube) {
-				printf("Invalid argument, already got a initial cube.\n");
+			if (!initial_cube) {
+				initial_cube = *argv;
+			} else if (!target_cube) {	
+				target_cube = *argv;
+			} else {
+				printf("Invalid argument, too many cube arguments.\n");
 				return -1;
 			}
-			initial_cube = *argv;
-			
 		} else {
 			printf("Invalid argument.\n");
 			return -1;
 		}
 	}
 	
-	if (!sequence) {
-		printf("Not enough arguments.\n");
-		return -1;
-	}
-	
-	if (!initial_cube) {
+	if (!sequence && initial_cube && !target_cube) {
+		target_cube = initial_cube;
+		initial_cube = DEFAULT_CUBE;
+	} else if (sequence && !initial_cube) {
 		initial_cube = DEFAULT_CUBE;
 	}
 	
+	if (!sequence && !initial_cube && !target_cube) {
+		printf("No arguments given.\n");
+		return -1;
+	}
 	
-
 	cube_t* cube = cube_create(initial_cube);
 	
-	cube_transform(cube, sequence);
+	if (sequence) {
+		cube_transform(cube, sequence);
+		printf("%s\n", cube->data);
+	} else {
+		
+		brute_cube(cube, target_cube, &brute_all_unsorted_print);
 	
-	printf("%s\n", cube->data);
-	
+	}
 	cube_free(cube);
 	
 	return 0;
